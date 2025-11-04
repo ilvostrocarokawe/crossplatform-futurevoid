0accac287a07ce48b2d8d2c959f009c9fd3f5e81
NicoMaker <nicola.marano02@gmail.com>
2025-11-04 10:53:54 +0100

    main

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:crossplatformfuturevoid/prova.dart';
+import 'package:crossplatformfuturevoid/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
